4a3a7d0d7c824841685980adc3e31cdb2bfe3e9a
Suvesh <anmolmoza2@gmail.com>
2022-08-15 17:24:14 +0530

    added navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, Navbar } from './components';
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<Header />
+		</>
+	);
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from './Footer';
+import Header from './Header';
+import Navbar from './Navbar';
+import Projects from './Projects';
+import Skills from './Skills';
+
+export { Footer, Header, Navbar, Projects, Skills };
